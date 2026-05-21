00b7682e084408eb809d220d2c7da01c3e1515be
apollo994 <fabio.zanarello.94@gmail.com>
2026-05-21 15:07:22 +0200

    clean up

--- scripts/get_intergenic_intervals_commands.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-shopt -s nullglob
-
-for gff in data/*/*.aliasMatch.gff{,3}; do
-  out="${gff}.intergenic.bed"
-  echo bash get_intergenic_intervals.sh "$gff" "$out"
-done
